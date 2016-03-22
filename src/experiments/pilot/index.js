09cfb16b038d5113c39b62c61b89d780c7b9c019
Fernando Flores <ferflores@gmail.com>
2016-03-22 12:06:17 -0600

    add box rain

--- src/experiments/pilot/index.js
+++ src/experiments/pilot/index.js
@@ -18,18 +18,17 @@
     body.frictionAir = 0;
   };
 
-
-
   var createBoxedWorld = function() {
     var screenStartX = 410;
     var screenStartY = 600;
-    
+
     var maxY = 640;
     var maxX = 800;
 
     var solid = 20;
 
     var floor = Bodies.rectangle(screenStartX+(solid/2), maxY+(solid*1.5), maxX-solid, solid, { isStatic: true });
+    floor.label = 'floor';
     removeFriction(floor);
     floor.restitution = 1.0;
 
@@ -49,11 +48,54 @@
   };
 
   // Actual logic
-  var box = Bodies.rectangle(400, 200, 80, 80);
-  removeFriction(box);
-  box.restitution = 1.765003; //almost...
+  var mainBox = Bodies.rectangle(400, 200, 80, 80);
+  removeFriction(mainBox);
+  mainBox.restitution = 1.765003; //almost...
 
-  World.add(engine.world, [box].concat(createBoxedWorld()));
+  var currentBoxes = [];
+
+  var boxId = 0;
+  function drawBoxes(){
+    if(currentBoxes.length < 200){
+      var newBox = Bodies.rectangle(Math.random() *500 + 100, Math.random() * 500 + 100, 10, 10, { frictionAir: .7 });
+      newBox.label = 'box'+boxId;
+      boxId++;
+      currentBoxes.push(newBox);
+      World.add(engine.world, newBox);
+    }
+
+    setTimeout(drawBoxes, 1);
+  }
+
+  World.add(engine.world, [mainBox].concat(createBoxedWorld()));
+
+  drawBoxes();
+
+  //Remove objects touching floor
+  Matter.Events.on(engine, 'collisionActive', function(e) {
+    for (var i = 0; i < e.pairs.length; i++) {
+      if(e.pairs[i].bodyA.label === 'floor'){
+        for (var j = 0; j < currentBoxes.length; j++) {
+          if(currentBoxes[j].label == e.pairs[i].bodyB.label){
+            currentBoxes.splice(j, 1);
+          }
+        }
+        World.remove(engine.world, e.pairs[i].bodyB);
+
+      }
+
+      if(e.pairs[i].bodyB.label === 'floor'){
+        for (var j = 0; j < currentBoxes.length; j++) {
+          if(currentBoxes[j].label == e.pairs[i].bodyA.label){
+            currentBoxes.splice(j, 1);
+          }
+        }
+        World.remove(engine.world, e.pairs[i].bodyA);
+
+      }
+    }
+  });
 
   Engine.run(engine);
+
 })();
