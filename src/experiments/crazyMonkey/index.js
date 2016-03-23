75cb31464562b6d3a0b790b4416a69941bd7fb2c
Fernando Flores <ferflores@gmail.com>
2016-03-23 11:57:41 -0600

    monkey faces

--- src/experiments/crazyMonkey/index.js
+++ src/experiments/crazyMonkey/index.js
@@ -23,6 +23,11 @@
 
       createScene();
       createMonkey();
+      listenEvents();
+      createHappyFaces();
+
+      var mouseConstraint = Matter.MouseConstraint.create(engine);
+      World.add(engine.world, [mouseConstraint]);
 
       var renderOptions = engine.render.options;
       renderOptions.background = '/experiments/crazyMonkey/img/background.jpg';
@@ -35,15 +40,55 @@
     }
 
     function createScene() {
-      var floor = Bodies.rectangle(sceneWidth/2, sceneHeight-100, sceneWidth, 10, { isStatic: true, render: { visible:false } });
+      var floor = Bodies.rectangle(-20, sceneHeight-60, sceneWidth*2, 10, { isStatic: true, render: { visible:false } });
       floor.label = 'floor';
-      World.add(engine.world, [floor]);
+
+      var leftWall = Bodies.rectangle(-5, 0, 10, sceneHeight*2, {isStatic: true, render:{visible:true}});
+      leftWall.label = 'wall';
+      leftWall.restitution = 1.00;
+
+      var rightWall = Bodies.rectangle(sceneWidth+5, 0, 10, sceneHeight*2, {isStatic: true, render:{visible:true}});
+      rightWall.label = 'wall';
+      rightWall.restitution = 1.00;
+
+      World.add(engine.world, [floor, leftWall, rightWall]);
     };
 
     function createMonkey() {
-      var monkey = Bodies.rectangle(200, 100, 100, 100, {render:{sprite:{texture:'/experiments/crazyMonkey/img/evilmonkey.png'}}});
+      var monkey = Bodies.rectangle(200, 300, 200, 200, {render:{sprite:{texture:'/experiments/crazyMonkey/img/evilmonkey.png'}}});
       monkey.label = 'monkey';
       World.add(engine.world, [monkey]);
+    }
+
+    function createHappyFaces(){
+      for(var x = 0; x< 150; x++){
+        var happyFace = Bodies.circle(Math.random() * sceneWidth, 0, 20, {render:{sprite:{texture:'/experiments/crazyMonkey/img/happyface.png', xScale:.3, yScale:.3}}});
+        happyFace.label = 'face';
+        World.add(engine.world, [happyFace]);
+      }
+    }
+
+    function listenEvents(){
+      Matter.Events.on(engine, 'collisionActive', function(e) {
+        for (var i = 0; i < e.pairs.length; i++) {
+
+          if(e.pairs[i].bodyA.label === 'monkey' && e.pairs[i].bodyB.label == 'face'){
+            e.pairs[i].bodyB.render.sprite.texture = '/experiments/crazyMonkey/img/sadface.png';
+          }
+
+          if(e.pairs[i].bodyB.label === 'monkey' && e.pairs[i].bodyA.label == 'face'){
+            e.pairs[i].bodyA.render.sprite.texture = '/experiments/crazyMonkey/img/sadface.png';
+          }
+
+          if(e.pairs[i].bodyA.label === 'wall'){
+            World.remove(engine.world, e.pairs[i].bodyB);
+          }
+
+          if(e.pairs[i].bodyB.label === 'wall'){
+            World.remove(engine.world, e.pairs[i].bodyA);
+          }
+        }
+      });
     }
 
 })();
